5303133cc3edad5a2e096fc3663edb2a6bacd9ca
Ioan Siran <ioansiran17@gmail.com>
2016-07-07 02:34:24 +0300

    Added cross platfrom functionality

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 #include <fstream>
-#include <windows.h>
 #include <cstdlib>
 #include <cstdio>
-#include<conio.h>
 #include <ctime>
+
+#ifdef _WIN32
+#include <windows.h>
+#include<conio.h>
+#define DIV 1048576 
+#define WIDTH 7
+#else
+#include <termios.h>
+static struct termios old, newt;
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old);
+  newt = old; 
+  newt.c_lflag &= ~ICANON; 
+  newt.c_lflag &= echo ? ECHO : ~ECHO; 
+  tcsetattr(0, TCSANOW, &newt); 
+}
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+char getch(void) 
+{
+  return getch_(0);
+}
+
+char getche(void) 
+{
+  return getch_(1);
+}
+#endif
 
 using namespace std;
 
@@ -12,10 +49,12 @@ int x=3,y=3;
 char mat[50][50];
 int score=0;
 
-int insertRandomPrizes(){
+void insertRandomPrizes(){
     int prizex=rand()%48+2;
     int prizey=rand()%19+2;
-    mat[prizey][prizex]='*';
+    if(mat[prizey][prizex]=='0')
+    	mat[prizey][prizex]='*';
+    else insertRandomPrizes();
 }
 char colors[]="ABCDEFGHIJ";
 void changeCollor(){
@@ -28,18 +67,26 @@ void changeCollorPostEnding(){
     colorCommand[6]=colors[rand()%5];
     system(colorCommand);
 }
-void printScreen(){
+void clear(){
+	#ifdef _WIN32
     system("cls");
-    for(int i=0;i<21;i++){
+    #else
+    system("clear");
+    #endif
+}
+void printScreen(){
+	clear();
+    for(int i=0;i<20;i++){
         for(int j=0;j<50;j++){
             if(x==j&&y==i){
                 if(mat[i][j]=='*'){
-
+                	#ifdef _WIN32
                     changeCollor();
+                    #endif
                     score++;
                     mat[i][j]='0';
                 }
-                cout<<(char)31;
+                cout<<'v';
             }
             else
                 if(mat[i][j]=='0')
@@ -48,7 +95,7 @@ void printScreen(){
                 if(mat[i][j]=='*')
                     cout<<'*';
 
-                else cout<<(char)219;
+                else cout<<'@';
             }
         }
         cout<<endl;
@@ -58,8 +105,8 @@ void printScreen(){
 void readMap(){
     ifstream mapInput("input.in");
     for(int i=0;i<50;i++){
-        f.mapInput(mat[i],51);
-
+        for(int j=0;j<50;j++)
+            mapInput>>mat[i][j];
     }
 }
 void moveUp(){
@@ -124,14 +171,10 @@ int main(){
 
         }
 
-    }while(true){
-        changeCollorPostEnding();
-        cout<<"*************************\n";
+   }  
+         cout<<"*************************\n";
         cout<<"**********WINNER*********\n";
         cout<<"*************************\n";
 
-        for(int i=0;i<100000000;i++);
-        system("cls");
-    }
     return 0;
 }
